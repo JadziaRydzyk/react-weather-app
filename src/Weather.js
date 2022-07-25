ce376c00a0748e6160124ed7976a44e9e5c049f1
JadziaRydzyk <jadzia.szy@gmail.com>
2022-07-25 12:58:16 +0200

    use correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       decription: response.data.weather[0].description,
       humidity: response.data.main.humidity,
-      iconUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
     });
   }
